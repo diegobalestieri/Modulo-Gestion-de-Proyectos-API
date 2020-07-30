baa09c4d10ed2cf7ab114c79f4bccebe0745323a
Manuel <manuelsturla@gmail.com>
2020-07-30 17:11:59 -0300

    Asignar ticket a una tarea

--- src/test/java/TestsProyecto/StepDefGestionarTarea.java
+++ src/test/java/TestsProyecto/StepDefGestionarTarea.java
@@ -22,8 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 import static java.lang.Long.parseLong;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -200,5 +199,56 @@ public class StepDefGestionarTarea extends SpringTest{
         String response = requestResult.getResponse().getContentAsString();
         Tarea nuevaTarea = mapper.readValue(response,Tarea.class);
         assertTrue(nuevaTarea.equals(auxTarea));
+    }
+
+
+    @When("se le asigna un ticket de soporte con codigo {int}")
+    public void seLeAsignaUnTicketDeSoporteConCodigo(int ticket) throws Exception {
+        String url_put = urlPostTarea.replace("{id}", idProyecto) + "/" + auxTarea.getId();
+        auxTarea.agregarTicket((long) ticket);
+        String requestJson = mapper.writeValueAsString(auxTarea);
+        MvcResult requestResult = this.mockMvc.perform(put(url_put)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Then("puedo ver los ids de los tickets de soporte asociados")
+    public void puedoVerLosIdsDeLosTicketsDeSoporteAsociados() throws Exception {
+        String url_get = urlPostTarea.replace("{id}", idProyecto) + "/" + auxTarea.getId();
+        MvcResult requestResult = this.mockMvc.perform(get(url_get)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String response = requestResult.getResponse().getContentAsString();
+        Tarea nuevaTarea = mapper.readValue(response,Tarea.class);
+        assertTrue(nuevaTarea.equals(auxTarea));
+    }
+
+    @When("se le desvincula un ticket de soporte con codigo {int}")
+    public void seLeDesvinculaUnTicketDeSoporteConCodigo(int ticket) throws Exception {
+        String url_put = urlPostTarea.replace("{id}", idProyecto) + "/" + auxTarea.getId();
+        auxTarea.eliminarTicket((long) ticket);
+        String requestJson = mapper.writeValueAsString(auxTarea);
+        MvcResult requestResult = this.mockMvc.perform(put(url_put)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Then("el ticket de codigo {int} ya no se encuentra asociado")
+    public void elTicketDeCodigoYaNoSeEncuentraAsociado(int ticket) throws Exception {
+        String url_get = urlPostTarea.replace("{id}", idProyecto) + "/" + auxTarea.getId();
+        MvcResult requestResult = this.mockMvc.perform(get(url_get)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String response = requestResult.getResponse().getContentAsString();
+        Tarea nuevaTarea = mapper.readValue(response,Tarea.class);
+        assertFalse(nuevaTarea.getIdsTickets().contains((long)ticket));
     }
 }
